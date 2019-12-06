f6019ea4188f908b313b46f48a47c55d560c489f
wesley_nunes <wesleysilva.ti@gmail.com>
2019-12-06 19:54:12 -0300

    Adicionar arquivos de projeto.

---
+++ LojaVirtualCore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtualCore.Controllers
+{
+    public class HomeController : Controller
+    {       
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtualCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaVirtualCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
